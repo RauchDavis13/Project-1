3ccab80761888755c8adc8b3663799b83c055917
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2021-12-04 18:51:59 -0500

    added sidebar nav for user menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,3 +128,15 @@ chuckBtn.addEventListener("click", getChuckJokes)
 dadBtn.addEventListener("click", getDadJokes)
 darkBtn.addEventListener("click", getGeekJokes)
 programBtn.addEventListener("click", getProgramJokes)
+
+// SIDE NAV BAR 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("main").style.marginLeft = "500px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
